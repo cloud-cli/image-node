9053cc71c4627c62054ac73a0498b98d0b704bb5
Darlan Alves <me@darlanalv.es>
2024-07-08 15:27:01 +0200

    fix: update entrypoint.sh

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -1,5 +1,7 @@
 cd /home/app
 
+set -e
+
 [[ -f superstatic.json ]] && node /home/node/superstatic.mjs
 [[ -f Procfile ]] && npx --yes foreman start --port $PORT
 [[ -f index.js ]] && node index.js
@@ -9,8 +11,9 @@ cd /home/app
 if [[ -f package.json ]]; then
   main=$(node -p -e 'require("./package.json").main||""')
   start=$(node -p -e 'require("./package.json").scripts?.start||""')
-  [[ -z "$main" ]] || node $main
-  [[ -z "$start" ]] || npm start
+
+  [[ ! -z "$main" ]] && node $main
+  [[ ! -z "$start" ]] && npm start
 fi
 
 echo "No entrypoint to execute. Exiting."
